6d5130ede7381a0db21e6265700c41e3e69696f3
Hong Nguyen <nguyentienhong1997@gmail.com>
2022-02-01 11:20:16 -0800

    hw1 final

---
+++ hw1/main.py
@@ -0,0 +1,11 @@
+class nearest_mean_classifier():
+    def __init__(self,data) -> None:
+        self.data = data
+    def fit(self, data):
+        return 0
+
+def main():
+    print("Hello World!")
+
+if __name__ == "__main__":
+    main()
